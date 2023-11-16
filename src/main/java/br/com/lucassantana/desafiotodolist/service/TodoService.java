a504174bfdefaacf753fef7559445dbc16da2efe
Lucas <lucas.desenvolvedor@hotmail.com>
2023-11-16 18:24:30 -0300

    Service Todo

---
+++ src/main/java/br/com/lucassantana/desafiotodolist/service/TodoService.java
@@ -0,0 +1,42 @@
+package br.com.lucassantana.desafiotodolist.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.lucassantana.desafiotodolist.entity.Todo;
+import br.com.lucassantana.desafiotodolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+    
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+
+    }
+
+
+
+}
